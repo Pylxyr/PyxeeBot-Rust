a056e25e3352b2af9e02f68493b70f95e19773fe
Pylxyr <flickteke@gmail.com>
2026-07-22 18:25:53 +0800

    Add files via upload

--- src/db/migrations/0002_last_voice_channel.sql
+++ src/db/migrations/0002_last_voice_channel.sql
@@ -1,2 +1,4 @@
--- NULL means not currently connected.
+-- Needed for restore_queue_on_restart: queue_snapshots already records
+-- *what* to restore, but not *where* to reconnect. NULL means "not
+-- currently connected" (or never connected).
 ALTER TABLE guild_settings ADD COLUMN last_voice_channel_id INTEGER;
